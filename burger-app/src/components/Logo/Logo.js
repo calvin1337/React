63444b7628439b4cdbf137672c8363895ca5c255
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-03-21 16:49:00 +0000

    toolbar

--- burger-app/src/components/Logo/Logo.js
+++ burger-app/src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import React from "react";
 import burgerLogo from "../../assets/images/burger-logo.png";
 import classes from "./logo.module.css";
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div style={{height:props.height}} className={classes.Logo}>
         <img src={burgerLogo} alt="MyBurger" />
     </div>
 );
