1ceb671ad9d3256137a9e4a4997842c191b1a1e2
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-03-21 20:02:11 +0000

    some improvments

--- burger-app/src/components/Burger/OrderSummery/OrderSummery.js
+++ burger-app/src/components/Burger/OrderSummery/OrderSummery.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 
 import Aux from "../../../hoc/Aux";
 
@@ -6,28 +6,40 @@ import Button from "../../UI/Button/Button";
 
 
 
-const orderSummery = ( props ) => {
-    const ingredientSummery = Object.keys( props.ingredients )
+class OrderSummery extends Component {
+
+    
+
+    render(){
+
+        const ingredientSummery = Object.keys( this.props.ingredients )
         .map( igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li> );
         } );
+        
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients</p>
+                <ul>
+                    {ingredientSummery}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success"clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        )
+    }
+}
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients</p>
-            <ul>
-                {ingredientSummery}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success"clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    )
-};
 
-export default orderSummery
+    
+
+   
+
+
+export default OrderSummery;
--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -109,7 +109,6 @@ class BurgerBuilder extends Component{
                     purchaseCancelled={this.purchaseCancelHandler}
                     purchaseContinued={this.purchaseContinueHandler} 
                     price={this.state.totalPrice}
-
                     />
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
