b9076c8961259a3361743b171f194192753cf456
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-03-27 14:58:33 +0000

    routing

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -3,11 +3,18 @@ import React, { Component } from 'react';
 import Posts from "./Posts/Posts";
 import { Route, NavLink, Switch, Redirect} from "react-router-dom";
 import './Blog.css';
-import NewPost from "./NewPost/NewPost"
+// import NewPost from "./NewPost/NewPost"
 // import FullPost from "./FullPost/FullPost";
+import asyncComponent from "../../hoc/asyncComponent";
+
+const AsyncNewPost = asyncComponent(() => {
+    return import("./NewPost/NewPost");
+});
+
+
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -17,7 +24,7 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink to="/posts/" exact>Posts</NavLink></li>
+                            <li><NavLink to="/posts" exact>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname:"/new-post",
                                 hash: "#submit",
@@ -29,7 +36,7 @@ class Blog extends Component {
                {/* <Route path="/" exact render={() => <h1>Home</h1>}/>
                 <Route path="/" render={() => <h1>Home 2</h1>}/> */}
                 <Switch>
-                {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null}
+                {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}
                 <Route path="/posts" component={Posts}/>
 
                 <Route render={() => <h1>Not found</h1>}/>
