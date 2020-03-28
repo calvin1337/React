b3652705318586b3cb5860701576629673c0030e
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-03-28 16:31:00 +0000

    contact form

--- burger-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-app/src/containers/Checkout/ContactData/ContactData.js
@@ -3,16 +3,66 @@ import Button from "../../../components/UI/Button/Button";
 import classes from "./contactData.module.css"
 import axios from "axios";
 import Spinner from "../../../components/UI/Spinner/Spinner";
-
+import Input from "../../../components/UI/Input/Input";
 
 class ContactData extends Component {
     state = {
-        name:"",
-        email: "",
-        address: {
-            street: "" ,
-            postalCode: ""
-        }, 
+        orderForm: {
+                name: {
+                    elementType:"input",
+                    elementConfig: {
+                        type:"text",
+                        placeholder:"Your Name"
+                    },
+                    value: ""
+                },
+                street: {
+                    elementType: "input",
+                    elementConfig: {
+                        type: "text",
+                        placeholder: "Street"
+                    },
+                    value: ""
+                },
+
+                postCode: {
+                    elementType: "input",
+                    elementConfig: {
+                        type: "text",
+                        placeholder: "Postcode"
+                    },
+                    value: ""
+                },
+                country: {
+                    elementType: "input",
+                    elementConfig: {
+                        type: "text",
+                        placeholder: "Country"
+                    },
+                    value: ""
+                },
+                email: {
+                    elementType: "input",
+                    elementConfig: {
+                        type: "email",
+                        placeholder: "Your E-Mail"
+                    },
+                    value: ""
+                },
+                deliveryMethod: {
+                    elementType: "select",
+                    elementConfig: {
+                        options:[
+
+                            {value:"fastest", displayValue: "Fastest"},
+                            {value:"cheapest", displayValue: "Cheapest"},
+
+                        ]
+                    },
+                    value: ""
+                },
+                      
+        },
         loading:false
     }
 
@@ -21,17 +71,7 @@ class ContactData extends Component {
         this.setState({loading:true});
         const order = {
             ingredients : this.props.ingredients ,
-            price: this.props.price ,
-            customer:{
-                name:"Calvin Donaldson",
-                address:{
-                    street: "Teststreet 1",
-                    postCode: "AB42",
-                    country: "Uk"
-                },
-                email: "test@test.com"
-            },
-            deliveryMethod:"fastest"
+            price: this.props.price 
         }
         axios.post("https://react-my-burger-13a65.firebaseio.com/orders.json", order)
         .then( response => {
@@ -45,13 +85,24 @@ class ContactData extends Component {
     }
 
     render () {
+        const formElementsArray = [];
+        for(let key in this.state.orderForm){
+            formElementsArray.push({
+                id:key,
+                config: this.state.orderForm[key]
+            });
+        }
 
         let form = (
                     <form>
-                    <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-                    <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-                    <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                    <input className={classes.Input} type="text" name="postal" placeholder="Postal" />
+                    {formElementsArray.map(formElement => (
+                        <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        />
+                    ))}
                     <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                 </form>
                 );
