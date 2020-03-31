b3d74614389f3dbf9f6f7ad7b96884e428f615a4
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-03-31 19:36:16 +0100

    handling asychronous code

--- redux_basics/src/index.js
+++ redux_basics/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {createStore, combineReducers} from "redux";
+import {createStore, combineReducers, applyMiddleware, compose} from "redux";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk"
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import counterReducer from "./store/reducers/counter";
@@ -14,8 +15,20 @@ const rootReducer = combineReducers({
     res: resultReducer
 });
 
+const logger = store => {
+    return next => {
+        return action => {
+            console.log("[Middleware] Dispatching", action);
+            const result = next(action);
+            console.log("[Middleware] next state", store.getState())
+            return result
+        }
+    }
+};
 
-const store = createStore(rootReducer);
+const composeEnhancers = compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 
 
--- redux_basics/src/store/action.js
+++
@@ -1,8 +0,0 @@
-
-
-export const INCREMENT = "INCREMENT";
-export const DECREMENT = "DECREMENT";
-export const ADD = "ADDFIVE";
-export const SUBTRACT = "SUBTRACTFIVE";
-export const STORE_RESULT = "STORE_RESULT";
-export const DELETE_RESULT = "DELETE_RESULT";
