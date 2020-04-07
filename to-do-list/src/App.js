779e8ba7d8721f545d83dca936f551ea828bde54
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-04-07 14:01:56 +0100

    added todo components

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
-
+import React, {Component} from 'react';
+import Todos from "./components/Todos";
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Make lunch",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Walk the dog",
+        completed: false
+      },
+    ]
+  }
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map(todo =>{
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      } 
+      return todo;
+    })});
+  }
+  render(){
+    return (
     <div className="App">
-      
+     <Todos todos={this.state.todos} markComplete={this.markComplete} />
     </div>
   );
+  }
+  
 }
 
 export default App;
