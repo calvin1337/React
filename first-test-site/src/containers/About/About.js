531b70703b6f40e97353ac8bb0b6b305eac4bc74
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-04-10 18:41:27 +0100

    small changes

--- first-test-site/src/containers/About/About.js
+++ first-test-site/src/containers/About/About.js
@@ -9,7 +9,7 @@ import style from "./about.module.css";
                 
                 <div className={style.Intro}>
 
-                Hello world
+                <p> Hello world! </p>
 
                 </div>
             
@@ -30,8 +30,40 @@ import style from "./about.module.css";
             
             
             
-            <div className={style.Example2}>World</div>
-            <div className={style.Example3}>Test</div>
+            <div className={style.Example2}>
+
+                 
+            <div className={style.card}>
+                    <figure>
+                        <img alt={"random from picsum"}src={"https://picsum.photos/200/300"} />
+                    </figure>
+                    <h2>Headline</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis.</p><br></br>
+
+                    <Link to="/" className={style.button}>Learn More</Link>
+                </div>
+
+
+            </div>
+
+
+
+            <div className={style.Example3}>
+
+             
+            <div className={style.card}>
+                    <figure>
+                        <img alt={"random from picsum"}src={"https://picsum.photos/200/300"} />
+                    </figure>
+                    <h2>Headline</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis.</p><br></br>
+
+                    <Link to="/" className={style.button}>Learn More</Link>
+                </div>
+
+
+
+            </div>
             
             </div>
             
