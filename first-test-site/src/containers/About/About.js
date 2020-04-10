ec6f63340af7db1c9da22a493a5569ca274f00db
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-04-10 17:32:40 +0100

    started playing around with some cards for the about page

--- first-test-site/src/containers/About/About.js
+++ first-test-site/src/containers/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 import style from "./about.module.css";
 
  function About() {
@@ -13,7 +14,22 @@ import style from "./about.module.css";
                 </div>
             
             
-            <div className={style.Example1}>Hello</div>
+            <div className={style.Example1}>
+                
+                <div className={style.card}>
+                    <figure>
+                        <img alt={"random from picsum"}src={"https://picsum.photos/200/300"} />
+                    </figure>
+                    <h2>Headline</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis.</p><br></br>
+
+                    <Link to="/" className={style.button}>Learn More</Link>
+                </div>
+
+            </div>
+            
+            
+            
             <div className={style.Example2}>World</div>
             <div className={style.Example3}>Test</div>
             
