04576af2ffb65682725e1fb3b0e588c99822ac1f
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-04-23 00:19:16 +0100

    added todo post

--- first-test-site/src/containers/Todo/Todo.js
+++ first-test-site/src/containers/Todo/Todo.js
@@ -1,12 +1,68 @@
 import React, { Component } from 'react';
 import styles from "./todo.module.css";
 import TodoItem from "../../components/TodoItem/TodoItem";
+import axios from "axios"
 
 export class Todo extends Component {
+
+    state = {
+        todos : [
+
+        ],
+        newTodo: ""
+    }
+
+    componentDidMount(){
+        axios.get("https://react-first-project-4e07c.firebaseio.com/Todo.json")
+        .then(res => {
+            const todos = [];
+            for(let key in res.data){
+                todos.push({
+                    ...res.data[key],
+                    id: key
+                });
+            }
+            this.setState({todos: todos})
+        })
+        
+       
+    }
+
+
 
     postClickHandler = () => {
         console.log("Hello")
     }
+
+    changedHandle = (e) => {
+      
+        if(e.keyCode === 13){
+            
+           let title = document.getElementById("todoInput");
+           this.addTodo(title.value);
+           title.value = ""
+        }
+
+       
+            
+    }
+
+    
+    
+
+
+    addTodo = (titleValue) => {
+        let data = {
+                title: titleValue,
+                completed: false
+        }
+
+        axios.post("https://react-first-project-4e07c.firebaseio.com/Todo.json" , data)
+        .then(res => console.log(res.data))
+        
+      }
+
+
 
     
     render() {
@@ -18,7 +74,7 @@ export class Todo extends Component {
                      <div className={styles.TodoListContainer}>
 
                          <div className={styles.inputContainer}>
-                         <input type="text" name="task" placeholder="Insert your task here..." />
+                         <input onKeyDown={(e) => this.changedHandle(e)} type="text" name="task" id="todoInput" placeholder="Insert your task here..." />
                          </div>
 
                         <div className={styles.TodoList}>
@@ -37,6 +93,10 @@ const h1Style = {
     textAlign: "center",
     fontSize: "40px",
     padding: "50px"
+    }
+
+    const displayNone = {
+        display: "none"
     }
 
 export default Todo
