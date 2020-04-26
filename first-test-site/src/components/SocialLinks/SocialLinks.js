3452a726580d90f1075aa200640ce8c1cff54338
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-04-26 19:12:25 +0100

    fixed some visual bugs

--- first-test-site/src/components/SocialLinks/SocialLinks.js
+++ first-test-site/src/components/SocialLinks/SocialLinks.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faTwitter, faGithub, faInstagram} from '@fortawesome/free-brands-svg-icons' 
-
+import styles from "./socialLinks.module.css"
  function SocialLinks() {
     return (
 
         <div style={inlineBlock}>
 
-    <FontAwesomeIcon style={style} icon={faFacebook} size="4x"/>
-    <FontAwesomeIcon style={style} icon={faTwitter} size="4x"/>
-    <FontAwesomeIcon style={style} icon={faGithub} size="4x"/>
-    <FontAwesomeIcon style={style} icon={faInstagram} size="4x"/>
+    <a className={styles.SocialBtn}><FontAwesomeIcon icon={faFacebook} size="4x"/></a>
+    <a className={styles.SocialBtn}><FontAwesomeIcon icon={faTwitter} size="4x"/></a>
+    <a className={styles.SocialBtn}><FontAwesomeIcon icon={faGithub} size="4x"/></a>
+    <a className={styles.SocialBtn}><FontAwesomeIcon icon={faInstagram} size="4x"/>
+ </a>
+
         
         </div>
     )
@@ -33,6 +35,7 @@ const inlineBlock = {
     width: "100%" ,
     margin: "auto" ,
     textAlign: "center",
-    background: "#222"
+    background: "#222",
+    paddingTop: "20px"
 
 }
