f680dcf3ad48202f2810ad91d1ca47a25b9517ea
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-04-28 04:22:45 +0100

    socials

--- first-test-site/src/components/SocialLinks/SocialLinks.js
+++ first-test-site/src/components/SocialLinks/SocialLinks.js
@@ -10,28 +10,28 @@ import styles from "./socialLinks.module.css"
  function SocialLinks() {
     return (
 
-        <div style={inlineBlock}>
+        <div  style={inlineBlock}>
 
-            <span className="fa-stack twt fa-2x">
+            <span className="fa-stack twt testingtwitter fa-2x">
                 <FontAwesomeIcon color="white"  style={widthIcons} className="fa-stack-1x" icon={faCircle}  />
                 <FontAwesomeIcon color="#1DA1F2"style={twitterOuter} className="fa-stack-1x twitter fa" icon={faCircle} />
                 <FontAwesomeIcon color="white"  style={twitterBird} className="fab fa-twitter fa-stack-1x" icon={faTwitter}  />
             </span>
             <span className="fa-stack git fa-2x">
-                <FontAwesomeIcon  color="white" className="fa-stack-1x" icon={faCircle}  />
+                <FontAwesomeIcon  color="white" className="fa-stack-1x tweet" icon={faCircle}  />
                 <FontAwesomeIcon  color="white" className="fa-stack-1x github fa" icon={faCircle} />
                 <FontAwesomeIcon  color="#222" className="fab fa-github fa-stack-1x" icon={faGithub}  />
             </span>
             <span className="fa-stack twt fa-2x">
                 <FontAwesomeIcon color="white"  style={widthIcons} className="fa-stack-1x" icon={faCircle}  />
-                <FontAwesomeIcon color="#1DA1F2" style={twitterOuter} className="fa-stack-1x twitter fa" icon={faCircle} />
-                <FontAwesomeIcon color="white"  style={twitterBird} className="fab fa-twitter fa-stack-1x" icon={faFacebook}  />
+                <FontAwesomeIcon color="white"  className="fa-stack-1x facebook fa" icon={faCircle} />
+                <FontAwesomeIcon color="#4267B2"  className="fab fa-facebook fa-stack-1x" icon={faFacebook}  />
             </span>
-            <span className="fa-stack twt fa-2x">
-                <FontAwesomeIcon color="white"   style={widthIcons}className="fa-stack-1x" icon={faCircle}  />
-                <FontAwesomeIcon color="#1DA1F2" style={twitterOuter} className="fa-stack-1x twitter fa" icon={faCircle} />
-                <FontAwesomeIcon color="white"  style={twitterBird} className="fab fa-twitter fa-stack-1x" icon={faInstagram}  />
-            </span>
+            <a><span className="fa-stack goo fa-2x">
+                <FontAwesomeIcon   style={widthIcons} className="fa-stack-1x insta1" icon={faCircle}  />
+                <FontAwesomeIcon  className="fa-stack-1x fa-circle insta fa" icon={faSquare} />
+                <FontAwesomeIcon  className="fab fa-instagram fa-stack-1x" icon={faInstagram}  />
+            </span></a>
         </div>
     )
 }
@@ -43,8 +43,8 @@ export default SocialLinks
 
 
 const style = {
-    color: "#eee",
-    margin: "10px",
+    color:"white",
+    borderRadius: "50%"
 
 }
 
@@ -53,19 +53,20 @@ const widthIcons = {
 }
 
 const twitterOuter = {
-    fontSize: "28px"
+    fontSize: "40px"
 }
 
 const twitterBird = {
-    fontSize: "21px"
+    fontSize: "30px"
 }
 
 const inlineBlock = {
-    display: "inline-block",
-    width: "100%" ,
+    display: "initial",
+    width: "500px" ,
     margin: "auto" ,
     textAlign: "center",
-    background: "#222",
-    paddingTop: "20px"
-
+    paddingTop: "20px",
+    height:"500px"
 }
+
+
