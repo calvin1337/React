c95b748dba9017557cf1ab1d278030033af8b920
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-04-29 22:03:24 +0100

    bootstrap navbar with router

--- marvelvsdc/src/App.js
+++ marvelvsdc/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./Components/Navbar/Navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+export class App extends Component {
 
-import './App.css';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  render() {
+
+    return (
+      <div className="app">
+        <Router>
+        <NavBar />
+
+        <Route path="/" exact render={props => (
+        <React.Fragment>
+        <h1>Test</h1>
+        </React.Fragment>
+      )} />
+      <Route path="/about" render={props => (
+        <h1>About</h1>
+      )} />
+        </Router>
+      </div>
+
+    )
+  }
 }
 
-export default App;
+export default App
