335bbf0c04e197e952f0b40073eaca0969912860
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-05-03 18:24:35 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react/marvelvsdc/precache-manifest.97fede9cec4cea1dbabbf41a3c50a46e.js"
+  "/React/marvelvsdc/precache-manifest.4f9459d0ee0f58f6b701acd7f7955676.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/marvelvsdc/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React/marvelvsdc/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
