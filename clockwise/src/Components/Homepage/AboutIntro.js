9b4cf0af4bd9bd9e87df8ab80b53031450751f13
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-05-09 00:50:02 +0100

    about us section

--- clockwise/src/Components/Homepage/AboutIntro.js
+++ clockwise/src/Components/Homepage/AboutIntro.js
@@ -8,13 +8,29 @@ function AboutIntro() {
             <Container>
                 <Row>
                     <Col md="6">
-                        <div className="sectionTitle">
-                            <span>About us</span>
-                            <h2>Who Are Clockwise Technologies</h2>
-                        </div>
+                    <div className="sectionTitle">
+                        <span>About us</span>
+                        <h2>Who are we</h2>
+                    </div>
+                    <div className="aboutInfo">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis quod fugit eius. Placeat, nobis error culpa consequatur voluptatum earum voluptatem!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, delectus molestiae atque architecto ad explicabo quasi est! Eligendi numquam repellat error? Repellat ullam, nesciunt porro at sunt ut eius quod.
+                        </p>
+                    </div>
+                        <button type="button" class="btn btn-outline-secondary">Find out more</button>
                     </Col>
                     <Col md="6">
-                        <h1>Testing</h1>
+                    <div className="img-container">
+                     <img src="https://images.unsplash.com/photo-1564182842519-8a3b2af3e228?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80" alt="" className="img img-responsive" />
+                     <img src="https://images.unsplash.com/photo-1558611997-dd5b20e08c71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="" className="img img-responsive" />
+                      <img src="http://themexriver.com/mega-industry/mega-industry/images/about-us/img-3.jpg" alt="" className="img img-responsive" />
+                    </div>
                     </Col>
                 </Row>
             </Container>
@@ -23,10 +39,11 @@ function AboutIntro() {
 }
 
 const sections = {
-    height: "500px",
+    height: "auto",
     padding:"100px"
 }
 
+// http://themexriver.com/mega-industry/mega-industry/
 
 export default AboutIntro
 
