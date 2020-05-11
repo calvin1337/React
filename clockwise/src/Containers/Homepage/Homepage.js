9080f24b46fc4490376f5373cb3a563b57168d7b
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-05-12 00:37:07 +0100

    updates to home

--- clockwise/src/Containers/Homepage/Homepage.js
+++ clockwise/src/Containers/Homepage/Homepage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import HomeSlider from "../../Components/Slider/HomeSlider";
 import AboutIntro from '../../Components/Homepage/AboutIntro';
 import Services from "../../Components/Services/services";
+import Contact from "../../Components/Homepage/Contact/Contact"
 
 function Homepage() {
     return (
@@ -10,6 +11,7 @@ function Homepage() {
             <HomeSlider />
             <AboutIntro />
             <Services />
+            <Contact />
         </div>
     )
 }
